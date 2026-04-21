30bac2dddf7e1603d7c4e98b5c205b556d783bbd
khushabudeore22 <khushideore2212@gmail.com>
2026-04-21 22:17:15 +0530

    basic structure added

---
+++ src/data.jsx
@@ -0,0 +1,3 @@
+const PROPERTIES = [];
+
+export { PROPERTIES };
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import toast from 'react-hot-toast';
+import './index.css'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+)
